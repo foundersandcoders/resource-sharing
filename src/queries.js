dfe53f493f7c491d750aefae3e417cbd507f3a68
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-12-01 11:55:12 +0000

    Add password-validating logic to query

--- src/queries.js
+++ src/queries.js
@@ -1,3 +1,4 @@
+const Bcrypt = require('bcrypt');
 const dbConn = require('../db_connection.js');
 
 const queries = {};
@@ -5,8 +6,8 @@ const queries = {};
 const convertToEndpoint = (Text) => {
   return Text
     .toLowerCase()
-    .replace(/ /g,'-')
-    .replace(/[^\w-]+/g,'');
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '');
 };
 
 queries.getTopics = (cb) => {
@@ -33,9 +34,9 @@ queries.createResource = (payload, cb) => {
   var endpoint = convertToEndpoint(payload.title);
   var values = [payload.title, payload.url, payload.topic, payload.typeid, 1, endpoint];
   var sql = `INSERT INTO resources(title, url, topic_id, type_id, user_id, endpoint)
-  VALUES ($1, $2, (SELECT id FROM topics WHERE endpoint = $3), $4, $5, $6)`
+  VALUES ($1, $2, (SELECT id FROM topics WHERE endpoint = $3), $4, $5, $6)`;
   dbConn.query(sql, values, (err) => {
-    if(err) cb(err);
+    if (err) cb(err);
     else {
       var redirect = `/${payload.topic}/${endpoint}`;
       cb(null, redirect);
@@ -43,5 +44,19 @@ queries.createResource = (payload, cb) => {
   });
 };
 
+queries.checkLogin = (payload, cb) => {
+  const username = payload.username;
+  const password = payload.password;
+  const values = [payload.username];
+  const sql = `SELECT * FROM users WHERE username = $1`;
+  dbConn.query(sql, data, (err) => {
+    if (err) cb(err);
+    const result = data.rows[0];
+    Bcrypt.compare(password, result.password, (err, isMatch) => {
+      if (err) cb(err);
+      cb(null, isMatch);
+    });
+  });
+};
 
 module.exports = queries;
